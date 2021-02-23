2fba670da3783a87725c26b693c40050bcd182c0
Bryan Lee <bryanrichie97@gmail.com>
2021-02-23 14:44:47 +1100

    feat(app) initial commit, set up the styling and base for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, NumberInput, NumberInputField, Spacer, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 function App() {
+  const [topLeftRadius, setTopLeftRadius] = useState('');
+  // const [topRightRadius, setTopRightRadius] = useState('');
+  // const [bottomLeftRadius, setbottomLeftRadius] = useState('');
+  // const [bottomRightRadius, setbottomRightRadius] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      bg="tomato"
+      minHeight="100vh"
+      justifyContent="center"
+    >
+      <NumberInput>
+        <NumberInputField
+          w={50}
+          mr={300}
+          bgColor="white"
+          onChange={(e) => {
+            setTopLeftRadius(e.target.value);
+            console.log(e.target.value);
+          }}
+        />
+        <NumberInputField w={50} ml={300} bgColor="white" />
+      </NumberInput>
+      <Flex
+        bg="turquoise"
+        h={600}
+        w={600}
+        alignItems="center"
+        justifyContent="center"
+        borderTopLeftRadius={topLeftRadius}
+        borderTopRightRadius={10}
+        borderBottomLeftRadius={15}
+        borderBottomRightRadius={20}
+      >
+        <Text fontWeight="bold" fontSize="5xl" color="white">
+          Border Radius Previewer
+        </Text>
+      </Flex>
+      <NumberInput>
+        <NumberInputField w={50} mr={300} bgColor="white" />
+        <NumberInputField w={50} ml={300} bgColor="white" />
+      </NumberInput>
+    </Flex>
   );
 }
 
